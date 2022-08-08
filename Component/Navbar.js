1f66ff3d7d7ccffaa9f2351dcc33778a90b63d33
unknown <ibrahimkhalil.mine@gmail.com>
2022-08-08 17:06:55 +0600

    footer and nav logo

--- Component/Navbar.js
+++ Component/Navbar.js
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai'
+import Link from 'next/link';
 import style from '../styles/Navbar.module.css'
+import { BsCartCheck } from 'react-icons/bs'
+import { AiOutlineHeart } from 'react-icons/ai'
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false)
-    const { navbar, logo, search, searchBar, menu, menubar, icon, anchor, listItem } = style
+    const { navbar, logo, menu, menubar, icon, listItem, link, span } = style
     return (
         <div className={navbar}>
             <div className={logo}>
                 <h2>FreshFood</h2>
             </div>
-            <div>
+            {/* <div>
                 <input type='text' className={searchBar} ></input ><span className={search}>Search</span>
-            </div>
+            </div> */}
             <div >
                 <AiOutlineMenu className={icon} ></AiOutlineMenu>
 
                 <div className={`${menu} ${menubar}`}>
-                    <li className={listItem}><a className={anchor} href='#home'>Home</a></li>
-                    <li className={listItem}><a className={anchor} href='#product'>Product</a></li>
-                    <li className={listItem}><a className={anchor} href='#about'>About</a></li></div>
+                    <li className={listItem}><Link className={link} href=''>
+                        <span className={span} title='Add to cart'>
+                            <BsCartCheck></BsCartCheck>
+                        </span>
+                    </Link></li>
+                    <li className={listItem}><Link className={span} href=''>
+                        <span className={span} title='Add to cart'>                                  <AiOutlineHeart></AiOutlineHeart>
+                        </span>
+                    </Link></li>
+                    <li className={listItem}><Link href='/'>Home</Link></li>
+                    <li className={listItem}><Link href='#product'>Product</Link></li>
+                    <li className={listItem}><Link href='#about'>About</Link></li>
 
+                    <li className={listItem}><Link href='login'>Login</Link></li>
+                </div>
             </div>
         </div >
     );
---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../Component/Footer';
+import Navbar from '../Component/Navbar';
+
+const Login = () => {
+    return (
+
+        <div>
+            <Navbar></Navbar>
+            <div>
+
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Login;
